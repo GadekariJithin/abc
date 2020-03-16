c411e5fd2efb944c38fbbe9a8d8701ee57c4f447
GadekariJithin <61520072+GadekariJithin@users.noreply.github.com>
2020-03-16 12:52:37 +0530

    Update SOService.java

--- SOService.java
+++ SOService.java
@@ -1,67 +1,101 @@
 package com.cts.soportal;
 
+import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class SOService {
-	
-	
-		
-		
-		@Autowired
-		SODao soDao;
-		
-	public void save(SODetail soDetail) {
-		soDetail.setDay1(getdate(soDetail.getDay1()))
-		soDetail.setDay2(getdate(soDetail.getDay2()))
-			soDetail.setDay3(getdate(soDetail.getDay3()))
-			soDao.save(soDetail);
-		}
-		
-        public Date getdate(String dateinstring){
-		Date dateinDate=new SimpleDateFormat("dd/MM/yyyy").parse(dateinstring);
-		return dateinDate;
-	       }
-	
-		public MstrUser findByLoginId(String loginId) {
-			return soDao.findByLoginId(loginId);
-		}
+       
+       
+              
+              
+              @Autowired
+              SODao soDao;
+              
+       public void save(SODetail soDetail) throws ParseException {
+                     soDao.save(soDetail);
+              }
+              
 
-		/*public List<MstrStatus> findByStatusType(String statusType) {
-			// TODO Auto-generated method stub
-			return soDao.findByStatusType(statusType);
-		}*/
+              public MstrUser findByLoginId(String loginId) {
+                     return soDao.findByLoginId(loginId);
+              }
 
-		
+              /*public List<MstrStatus> findByStatusType(String statusType) {
+                     // TODO Auto-generated method stub
+                     return soDao.findByStatusType(statusType);
+              }*/
 
-		public List<MstrStatus> findByStatusType(String statusType1, String statusType2) {
-			return soDao.findByStatusType(statusType1, statusType2);
-		}
+              
 
-		
+              public List<MstrStatus> findByStatusType(String statusType1, String statusType2) {
+                     return soDao.findByStatusType(statusType1, statusType2);
+              }
 
-		
+              
 
-		
-		public List<MstrResourceLocation> findByLocation() {
-			// TODO Auto-generated method stub
-			return soDao.findByLocation();
-		}
+              
 
-		public List<MstrLob> findByLob() {
-			// TODO Auto-generated method stub
-			return soDao.findByLob();
-		}
+              
+              public List<MstrResourceLocation> findByLocation() {
+                     // TODO Auto-generated method stub
+                     return soDao.findByLocation();
+              }
 
-		
-		
+              public List<MstrLob> findByLob() {
+                     // TODO Auto-generated method stub
+                     return soDao.findByLob();
+              }
 
-		
 
-		
+              public List<SODetail> findBySOId() {
+                     // TODO Auto-generated method stub
+                     return soDao.findBySOId();
+              }
 
-	}
+
+              public List<MstrWorkOrder> findByProject() {
+                     // TODO Auto-generated method stub
+                     return soDao.findByProject();
+              }
+
+
+              public List<MstrSkill> findBySkill() {
+                     // TODO Auto-generated method stub
+                     return soDao.findBySkill();
+              }
+
+
+              public List<SOPractiseArea> findByPractice() {
+                     // TODO Auto-generated method stub
+                     return soDao.findByPractice();
+              }
+              public List<SOPractise> findBydl() {
+                     // TODO Auto-generated method stub
+                     return soDao.findBydl();
+              }
+
+
+              public List<SODetail> fetchData() {
+                     // TODO Auto-generated method stub
+                     return soDao.fetchData();
+              }
+
+
+              
+
+              
+
+              
+              
+
+              
+
+              
+
+       }
+
 
 
